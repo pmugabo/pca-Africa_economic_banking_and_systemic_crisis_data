000a0101b4bd920b142ccbcfcac7b7ecc005c7e6
MikeManzi <manzimike378@gmail.com>
2025-06-11 21:32:29 +0200

    add setup

---
+++ matrix-calc-package/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="matrix_calc",
+    version="1.0.0",
+    description="A matrix multiplication package by Peer Group 5.",
+    author="Patricia Mugabo, Carine Ashimwe, Daniel Marial Reng Kudum, Mike Manzi",
+    author_email="m.mike@alustudent.com",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.6"
+)
